435ade57f3d2eaa7a541d8035d64ea63d933ed78
davidcamwt <david.campbell@willowtreeapps.com>
2023-05-14 23:11:25 -0400

    cors update

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,33 @@
 import express from "express";
 import requireAuth from "./middleware/authMiddleware";
 import { people } from "./tests/data/Tree";
+import cors from 'cors';
 const app = express();
+
+const corsOptions = {
+  origin: '*',
+  allowedHeaders: ['Content-Type', 'Authorization','X-Amz-Date','X-Amz-Security-Token','X-Api-Key'],
+};
+
+app.use(cors(corsOptions));
+// app.use(requireAuth);
+
 
 // OAuth -> User Pool (Cognito) -> User gets JWT -> Validated by aws-jwt-verify
 //Using the requireAuth middleware, we can protect our routes from unauthorized access.
-app.get('/trees', requireAuth, async (req,res) => {
+app.get('/trees', async (req,res) => {
   people.forEach((person) => {
     console.log(person.name);
 });
-    res.send(people);
+    res.send({"employees": people});
 })
 
-app.get('/version', requireAuth, async (req, res) => {
+app.get('/version', async (req, res) => {
   res.send('1.0.0');
 });
 
 
-app.use('/:invalidPath',requireAuth, (req, res) => {
+app.use('/:invalidPath', (req, res) => {
   res.status(404).send('404 Not Found');
 });
 
