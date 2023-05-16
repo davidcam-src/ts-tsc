1de5f208f8f8dfbb53fe7c6b508f305a66322620
davidcamwt <david.campbell@willowtreeapps.com>
2023-05-16 10:22:36 -0400

    adding typescript library for testing to module

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
