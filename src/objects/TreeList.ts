4cc2dce39942b1730aa823a64a98560265ec448e
davidcamwt <david.campbell@willowtreeapps.com>
2023-05-20 16:18:38 -0400

    moving functions

--- src/objects/TreeList.ts
+++ src/objects/TreeList.ts
@@ -8,24 +8,142 @@ export class TreeList {
   public async addBambooEmployeeList(bambooService: BambooService) {
     const bambooDirectory = await bambooService.getBambooDirectory();
     this.emailToTreeMap = Object.fromEntries(
-      bambooService.generateEmailToTreeMap(bambooDirectory as object[]),
+      this.generateEmailToTreeMap(bambooDirectory as object[]),
     );
   }
 
   public async addBambooCustomReport(bambooService: BambooService) {
-    const bambooCustomReport = await bambooService.getBambooCustomReport();
-    bambooService.addBambooCustomReport(
-      bambooCustomReport as object[],
-      this.emailToTreeMap,
-    );
+    const employeesFromCustomReport =
+      (await bambooService.getBambooCustomReport()) as object[];
+    const existingEmailToTreeList = this.emailToTreeMap;
+
+    employeesFromCustomReport.forEach((employee) => {
+      const workEmail = employee['workEmail'];
+      if (workEmail) {
+        const workEmailAsLowerCase = (workEmail as string).toLowerCase();
+
+        if (existingEmailToTreeList['workEmailAsLowerCase']) {
+          console.log('Multiple trees in bamboo with the same email.');
+
+          let existingTree = existingEmailToTreeList.get(workEmailAsLowerCase);
+
+          existingTree['supervisorId'] = employee['supervisorEId'];
+          existingTree['supervisorEmail'] = employee['supervisorEmail'];
+          existingTree['city'] = employee['city'];
+          existingTree['state'] = employee['state'];
+          existingTree['pronunciation'] =
+            employee['customNamePronunciation(PhoneticSpelling)'];
+          existingTree['hireDate'] = employee['hireDate'];
+          existingTree['pronouns'] = employee['customPronoun'];
+          existingTree['newHireMentor'] = employee['customNewHireMentor'];
+          existingTree['longTermMentor'] = employee['customLongTermMentor'];
+          existingTree['state'] =
+            existingTree['state'] == ''
+              ? (existingTree['state'] = null)
+              : undefined;
+        }
+      }
+    });
   }
 
-  // public async addBambooPtoList(bambooService: BambooService) {
-  //   const bambooWhosOutList = await bambooService.getBambooWhosOutList();
-  //   this.emailToTreeMap = new Map<string, Object>();
-  //   bambooService.addBambooCustomReport(
-  //     bambooCustomReport as object[],
-  //     this.emailToTreeMap as object,
-  //   );
-  // }
+  public async addBambooPtoList(bambooService: BambooService) {
+    const bambooWhosOutList =
+      (await bambooService.getBambooWhosOutList()) as object[];
+    bambooWhosOutList.forEach((employee) => {
+      let treeOnPTO = employee['employeeId'];
+      // Checkpoint: Writing from python file in progress
+    });
+  }
+
+  public generateEmailToTreeMap(
+    employeesFromBambooDirectory: object[],
+  ): Map<string, Object> {
+    const emailToTreeMap = new Map<string, Object>();
+
+    employeesFromBambooDirectory.forEach((employee) => {
+      const workEmail = employee['workEmail'];
+      if (workEmail) {
+        const workEmailAsLowerCase = (workEmail as string).toLowerCase();
+
+        if (emailToTreeMap.has(workEmailAsLowerCase)) {
+          console.log('Multiple trees in bamboo with the same email.');
+        }
+        emailToTreeMap.set(
+          workEmailAsLowerCase,
+          this.bambooObjectToTree(employee, workEmailAsLowerCase) as Object,
+        );
+      }
+    });
+    return emailToTreeMap;
+  }
+
+  public bambooObjectToTree(
+    employee: object,
+    workEmailAsLowerCase: string,
+  ): Object {
+    const tree = new Tree();
+
+    tree.id = employee['id'];
+    tree.name = employee['displayName'];
+    tree.firstName = employee['firstName'];
+    tree.lastName = employee['lastName'];
+    tree.preferredName = employee['preferredName'] || null;
+    tree.displayName = employee['displayName'];
+    tree.department = employee['department'] || null;
+    tree.jobTitle = employee['jobTitle'] || null;
+    tree.location = employee['location'] || 'NA';
+    tree.pronouns = employee['pronouns'] || null;
+    tree.photoUrl = employee['photoUrl'] || null;
+    tree.supervisor = employee['supervisor'] || null;
+    tree.allocations = employee['allocations'];
+    // tree.skills = employee['skills'];
+    tree.pronunciation = employee['pronunciation'] || null;
+    tree.branch = employee['branch'] || null;
+    tree.employeeStatus = 'default';
+    tree.PTOStart = null;
+    tree.PTOEnd = null;
+    tree.hireDate = employee['hireDate'] || null;
+    tree.supervisorId = employee['supervisorId'] || null;
+    tree.supervisorEmail = employee['supervisorEmail'] || null;
+    tree.email = workEmailAsLowerCase;
+    tree.allocations = [];
+
+    if (!tree.preferredName || tree.preferredName === tree.lastName) {
+      tree.displayName = tree.name;
+    } else {
+      tree.displayName = `${tree.preferredName} ${tree.lastName}`;
+    }
+
+    const retVal = {
+      id: employee['id'],
+      name: employee['displayName'],
+      firstName: employee['firstName'],
+      lastName: employee['lastName'],
+      preferredName: employee['preferredName'] || null,
+      displayName: tree.displayName,
+      department: employee['department'] || null,
+      jobTitle: employee['jobTitle'] || null,
+      location: employee['location'] || 'NA',
+      pronouns: employee['pronouns'] || null,
+      photoUrl: employee['photoUrl'] || null,
+      supervisor: employee['supervisor'] || null,
+      allocations: tree.allocations,
+      skills: [],
+      pronunciation: employee['pronunciation'] || null,
+      branch: employee['branch'] || null,
+      employeeStatus: 'default',
+      PTOStart: null,
+      PTOEnd: null,
+      hireDate: employee['hireDate'] || null,
+      supervisorId: employee['supervisorId'] || null,
+      supervisorEmail: employee['supervisorEmail'] || null,
+      email: workEmailAsLowerCase,
+      state: employee['state'] || null,
+      city: employee['city'] || null,
+      newHireMentor: employee['newHireMentor'] || null,
+      longTermMentor: employee['longTermMentor'] || null,
+      slackId: employee['slackId'] || null,
+    };
+    return retVal;
+  }
 }
--- src/services/bamboo.service.ts
+++ src/services/bamboo.service.ts
@@ -31,45 +31,8 @@ export class BambooService {
       },
     );
 
-    console.log(JSON.stringify(response));
-
     const customReportList: any[] = response.data['employees'];
     return customReportList;
-  }
-
-  public addBambooCustomReport(
-    employeesFromCustomReport: object[],
-    existingEmailToTreeList: any,
-  ) {
-    employeesFromCustomReport.forEach((employee) => {
-      const workEmail = employee['workEmail'];
-      if (workEmail) {
-        const workEmailAsLowerCase = (workEmail as string).toLowerCase();
-
-        if (existingEmailToTreeList['workEmailAsLowerCase']) {
-          console.log('Multiple trees in bamboo with the same email.');
-
-          let existingTree = existingEmailToTreeList.get(workEmailAsLowerCase);
-
-          existingTree['supervisorId'] = employee['supervisorEId'];
-          existingTree['supervisorEmail'] = employee['supervisorEmail'];
-          existingTree['city'] = employee['city'];
-          existingTree['state'] = employee['state'];
-          existingTree['pronunciation'] =
-            employee['customNamePronunciation(PhoneticSpelling)'];
-          existingTree['hireDate'] = employee['hireDate'];
-          existingTree['pronouns'] = employee['customPronoun'];
-          existingTree['newHireMentor'] = employee['customNewHireMentor'];
-          existingTree['longTermMentor'] = employee['customLongTermMentor'];
-          existingTree['state'] =
-            existingTree['state'] == ''
-              ? (existingTree['state'] = null)
-              : undefined;
-        }
-      }
-    });
-
-    return existingEmailToTreeList;
   }
 
   public async getBambooWhosOutList(): Promise<object> {
@@ -93,97 +56,5 @@ export class BambooService {
     );
     const employeesList: any[] = response.data['employees'];
     return employeesList;
-  }
-
-  public generateEmailToTreeMap(
-    employeesFromBambooDirectory: object[],
-  ): Map<string, Object> {
-    const emailToTreeMap = new Map<string, Object>();
-
-    employeesFromBambooDirectory.forEach((employee) => {
-      const workEmail = employee['workEmail'];
-      if (workEmail) {
-        const workEmailAsLowerCase = (workEmail as string).toLowerCase();
-
-        if (emailToTreeMap.has(workEmailAsLowerCase)) {
-          console.log('Multiple trees in bamboo with the same email.');
-        }
-        emailToTreeMap.set(
-          workEmailAsLowerCase,
-          this.bambooObjectToTree(employee, workEmailAsLowerCase) as Object,
-        );
-      }
-    });
-    return emailToTreeMap;
-  }
-
-  public bambooObjectToTree(
-    employee: object,
-    workEmailAsLowerCase: string,
-  ): Object {
-    const tree = new Tree();
-
-    tree.id = employee['id'];
-    tree.name = employee['displayName'];
-    tree.firstName = employee['firstName'];
-    tree.lastName = employee['lastName'];
-    tree.preferredName = employee['preferredName'] || null;
-    tree.displayName = employee['displayName'];
-    tree.department = employee['department'] || null;
-    tree.jobTitle = employee['jobTitle'] || null;
-    tree.location = employee['location'] || 'NA';
-    tree.pronouns = employee['pronouns'] || null;
-    tree.photoUrl = employee['photoUrl'] || null;
-    tree.supervisor = employee['supervisor'] || null;
-    tree.allocations = employee['allocations'];
-    // tree.skills = employee['skills'];
-    tree.pronunciation = employee['pronunciation'] || null;
-    tree.branch = employee['branch'] || null;
-    tree.employeeStatus = 'default';
-    tree.PTOStart = null;
-    tree.PTOEnd = null;
-    tree.hireDate = employee['hireDate'] || null;
-    tree.supervisorId = employee['supervisorId'] || null;
-    tree.supervisorEmail = employee['supervisorEmail'] || null;
-    tree.email = workEmailAsLowerCase;
-    tree.allocations = [];
-
-    if (!tree.preferredName || tree.preferredName === tree.lastName) {
-      tree.displayName = tree.name;
-    } else {
-      tree.displayName = `${tree.preferredName} ${tree.lastName}`;
-    }
-
-    const retVal = {
-      id: employee['id'],
-      name: employee['displayName'],
-      firstName: employee['firstName'],
-      lastName: employee['lastName'],
-      preferredName: employee['preferredName'] || null,
-      displayName: tree.displayName,
-      department: employee['department'] || null,
-      jobTitle: employee['jobTitle'] || null,
-      location: employee['location'] || 'NA',
-      pronouns: employee['pronouns'] || null,
-      photoUrl: employee['photoUrl'] || null,
-      supervisor: employee['supervisor'] || null,
-      allocations: tree.allocations,
-      skills: [],
-      pronunciation: employee['pronunciation'] || null,
-      branch: employee['branch'] || null,
-      employeeStatus: 'default',
-      PTOStart: null,
-      PTOEnd: null,
-      hireDate: employee['hireDate'] || null,
-      supervisorId: employee['supervisorId'] || null,
-      supervisorEmail: employee['supervisorEmail'] || null,
-      email: workEmailAsLowerCase,
-      state: employee['state'] || null,
-      city: employee['city'] || null,
-      newHireMentor: employee['newHireMentor'] || null,
-      longTermMentor: employee['longTermMentor'] || null,
-      slackId: employee['slackId'] || null,
-    };
-    return retVal;
   }
 }
